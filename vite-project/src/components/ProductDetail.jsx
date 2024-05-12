e4854c15b8e426be8fce2db932476bb013fef1c1
Afsana <abdullayevaynur527@gmail.com>
2024-05-12 13:50:53 +0400

    trash

--- vite-project/src/components/ProductDetail.jsx
+++ vite-project/src/components/ProductDetail.jsx
@@ -2,11 +2,11 @@ import { useParams } from 'react-router-dom';
 import { useGetProductByIdQuery } from '../services/api'; 
 
 function ProductDetail() {
-  const { id } = useParams(); // Parametre adının doğru olduğundan emin olun
-  const { data: product, error, isLoading } = useGetProductByIdQuery(id); // Ürün sorgusu doğru bir şekilde yapılıyor mu?
+  const { id } = useParams(); 
+  const { data: product, error, isLoading } = useGetProductByIdQuery(id); 
 
   if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>; // Hata mesajını error.message olarak gösterin
+  if (error) return <div>{error.message}</div>; 
   if (!product) return <div>No product found</div>; 
 
   return (
